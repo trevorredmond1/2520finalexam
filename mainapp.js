4590dd08acad50abdb1dcf0077b5f6fa3b41c2d0
trevorredmond1 <trevor.redmond19@gmail.com>
2018-04-15 23:46:00 -0700

    yeet

---
+++ mainapp.js
@@ -0,0 +1,34 @@
+//constants
+
+const hbs = require('hbs');
+const express = require('express');
+const port = process.env.PORT || 8080;
+const bodyparser = require('body-parser');
+
+//static content
+
+var app = express();
+var urlencodedParser = bodyparser.urlencoded({extended: false});
+
+app.use(express.static(__dirname + '/views'));
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+//main page
+
+app.get('/', (request, response) => {
+  response.render('mainapp.hbs');
+});
+
+app.get('/login', (request, response) => {
+  response.render('login.hbs');
+});
+
+app.post('/verifylogin', urlencodedParser, (request, response) => {
+  console.log(request.body.user, request.body.pass)
+  res.redirect('/')
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
